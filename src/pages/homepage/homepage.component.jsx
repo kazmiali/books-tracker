e3d1527efbdd3d3b098f0255b6480bc28625b2f9
Muhammad Ali Kazmi <ali.smak099@gmail.com>
2019-10-02 00:53:41 +0500

    Changed HomePage Component code

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const HomePage = () => {
-	return (
-		<div>
-			<h1>HomePage</h1>
-			<Link to='/signup'>Go to Sign Up</Link>
-			<Link to='/login'>Go to Sign In</Link>
-		</div>
-	);
-};
-
-export default HomePage;
+const HomePage = () => (
+	<div className=''>
+		<p>HomePage</p>
+	</div>
+);
